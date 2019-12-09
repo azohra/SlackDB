72735b1d60f5ee903de61544e7ef7c611258a785
Borna Houmani-Farahani <borna.houmanifarahani6@loblaw.ca>
2019-12-09 16:54:46 -0500

    channels rework

---
+++ test/channels_test.exs
@@ -0,0 +1,54 @@
+defmodule SlackDB.ChannelsTest do
+  use ExUnit.Case
+
+  import Mox
+  import SlackDB.Channels
+
+  @pagination_result_1 %{
+    "channels" => [
+      %{
+        "id" => "CQ5H0JBDZ",
+        "name" => "general"
+      },
+      %{
+        "id" => "CQ5H0K15Z",
+        "name" => "slackdb"
+      }
+    ],
+    "ok" => true,
+    "response_metadata" => %{"next_cursor" => "cursor1"}
+  }
+
+  @pagination_result_2 %{
+    "channels" => [
+      %{
+        "id" => "CQ5H0JBDZ",
+        "name" => "new"
+      }
+    ],
+    "ok" => true,
+    "response_metadata" => %{"next_cursor" => ""}
+  }
+
+  setup do
+    Client.Mock
+    |> expect(:conversations_list, 6, fn
+      _, nil -> {:ok, @pagination_result_1}
+      _, "cursor1" -> {:ok, @pagination_result_2}
+    end)
+
+    :ok
+  end
+
+  test "conversations_list" do
+    assert get_all_convos("server") ===
+             {:ok,
+              [
+                %{"id" => "CQ5H0JBDZ", "name" => "general"},
+                %{"id" => "CQ5H0K15Z", "name" => "slackdb"},
+                %{"id" => "CQ5H0JBDZ", "name" => "new"}
+              ]}
+
+    assert get_all_convos("doesnt exist") === {:error, "improper_config"}
+  end
+end
